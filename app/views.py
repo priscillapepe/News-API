a380ddae5f4504d08750511e845419a250347431
priscillapepe <priscillaungai99@gmail.com>
2020-09-12 19:07:46 +0300

    added title and return to views.py

--- app/views.py
+++ app/views.py
@@ -3,9 +3,10 @@ from app import app
 
 # Views
 @app.route('/news/<news_title>')
-def news(news_title):
+def index():
 
     '''
     View news page function that returns the news details page and its data
     '''
-    return render_template('news.html',title = news_title)
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title)

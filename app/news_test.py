47ef3429446c18e22c51a973a3cd4e7bc7ec65fb
priscillapepe <priscillaungai99@gmail.com>
2020-09-12 22:57:54 +0300

    made changed and added some info into files

--- app/news_test.py
+++ app/news_test.py
@@ -1,17 +1,17 @@
 import unittest
-from models import News
-News = News.news
+from models import news
+News = news.News
 
 class NewsTest(unittest.TestCase):
     '''
-    Test Class to test the behaviour of the News class
+    Test Class to test the behaviour of News class
     '''
 
     def setUp(self):
         '''
         Set up method that will run before every Test
         '''
-        self.new_news = News(1234,'BBi News','KBC News','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
+        self.new_news = News(1234,'BBI News','BBC News','/khsjha27hbs',8.5,129993)
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
--- app/request.py
+++ app/request.py
@@ -1,7 +1,11 @@
 from app import app
+import urllib.request,json
+from .models import news
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
 
 def get_news(category):
     pass
+
+News = news.News

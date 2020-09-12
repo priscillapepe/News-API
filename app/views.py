c5814495e05d15147f6a40a6ef43a3f8a9b743dc
priscillapepe <priscillaungai99@gmail.com>
2020-09-12 17:59:42 +0300

    made changed

--- app/views.py
+++ app/views.py
@@ -3,9 +3,9 @@ from app import app
 
 # Views
 @app.route('/news/<news_title>')
-def movie(news_title):
+def news(news_title):
 
     '''
-    View movie page function that returns the movie details page and its data
+    View news page function that returns the news details page and its data
     '''
     return render_template('news.html',title = news_title)

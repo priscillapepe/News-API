5532f1cadf2820800921372b646a4276739358cf
priscillapepe <priscillaungai99@gmail.com>
2020-09-12 17:48:45 +0300

    made changes on views.py

--- app/views.py
+++ app/views.py
@@ -2,10 +2,10 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/news/<news_id>')
-def movie(news_id):
+@app.route('/news/<news_title>')
+def movie(news_title):
 
     '''
     View movie page function that returns the movie details page and its data
     '''
-    return render_template('news.html',id = news_id)
+    return render_template('news.html',title = news_title)

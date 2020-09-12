9a19b24c89c14ed737f793dc02f81f7d0398a543
priscillapepe <priscillaungai99@gmail.com>
2020-09-12 19:49:26 +0300

    updated file

---
+++ app/request.py
@@ -0,0 +1,7 @@
+from app import app
+
+# Getting api key
+api_key = app.config['NEWS_API_KEY']
+
+def get_news(category):
+    pass
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/news/<news_title>')
+@app.route('/')
 def index():
 
     '''
@@ -10,3 +10,12 @@ def index():
     '''
     title = 'Home - Welcome to The best News Review Website Online'
     return render_template('index.html', title = title)
+
+@app.route('/movie/<movie_id>')
+def news(news_title):
+
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    title = f'You are viewing {news_title}'
+    return render_template('news.html',title = title)

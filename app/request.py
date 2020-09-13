6db64877776a7f2306bc8d42f7dbd90e8d4fda12
priscillapepe <priscillaungai99@gmail.com>
2020-09-13 12:27:36 +0300

    updated files

--- app/request.py
+++ app/request.py
@@ -1,11 +1,55 @@
 from app import app
-import urllib.request,json
+import urllib.request
+import json
 from .models import news
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
 
-def get_news(category):
-    pass
+# Getting the movie base url
+base_url = app.config["NEWS_API_BASE_URL"]
 
 News = news.News
+
+
+def get_news(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = base_url.format(category, api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+    if get_news_response['results']:
+        news_results_list = get_news_response['results']
+        news_results = process_results(news_results_list)
+
+    return news_results
+
+    def process_results(news_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+
+    Args:
+        movie_list: A list of dictionaries that contain news details
+
+    Returns :
+        news_results: A list of news objects
+    '''
+    news_results = []
+    for news_item in movie_list:
+        title = news_item.get('original_title')
+        overview = news_item.get('overview')
+        poster = news_item.get('poster_path')
+        vote_average = news_item.get('vote_average')
+        vote_count = news_item.get('vote_count')
+
+        if poster:
+            news_object = News(title,overview,poster,vote_average,vote_count)
+            news_results.append(news_object)
+
+    return movie_results
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 # Views
 @app.route('/')
@@ -8,10 +9,14 @@ def index():
     '''
     View news page function that returns the news details page and its data
     '''
+    
+    # Getting popular news
+    popular_news = get_news('popular')
+    upcoming_news = get_news('upcoming')
+    now_showing_news = get_news('now_playing')
     title = 'Home - Welcome to The best News Review Website Online'
-    return render_template('index.html', title = title)
-
-@app.route('/movie/<movie_id>')
+    return render_template('index.html', title = title, popular = popular_news, upcoming = upcoming_news, now_showing = now_showing_news )
+@app.route('/news/<news_title>')
 def news(news_title):
 
     '''

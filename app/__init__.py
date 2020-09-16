7931edf4bac58fbc894f9007c91c0a55c480736d
priscillapepe <priscillaungai99@gmail.com>
2020-09-16 09:07:19 +0300

    deployment to heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template
-from instance.config import DevConfig
+# from instance.config import DevConfig
 from flask_bootstrap import Bootstrap
 from config import config_options
 bootstrap = Bootstrap()
 
 
-# def create_app(config_name):
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__)
 # Creating the app configurations
-app.config.from_object(config_options['development'])
+    app.config.from_object(config_options['development'])
 # config_options[config_name].init_app(app)
 # Initializing flask extensions
-bootstrap.init_app(app)
+    bootstrap.init_app(app)
 # Registering the blueprint
-from .main import main as main_blueprint
-app.register_blueprint(main_blueprint)
-from app import views
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    # from app import views
 # setting config
-# return app
+    return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,14 @@
-from app import app
+# from app import app
+from . import main
 from flask import render_template
-from app.requests import NewsRequests
+from ..requests import NewsRequests
 # from run import app
 # from app import create_app
 
 # app = create_app('development')
 request = NewsRequests()
 
-@app.route('/')
+@main.route('/')
 def index():
     topheadlines = request.get_top_headlines(sources='bbc-news')
     # return topheadlines
@@ -15,14 +16,14 @@ def index():
     return render_template('index.html', data = topheadlines['articles'])       
 
 
-@app.route('/fox')
+@main.route('/fox')
 def fox():
     topheadlines = request.get_top_headlines(sources='fox-news')  
     # return topheadlines
 
     return render_template('fox.html', data = topheadlines['articles']) 
     
-@app.route('/abc')
+@main.route('/abc')
 def abc():
     topheadlines = request.get_top_headlines(sources='abc-news')  
     print(topheadlines)
